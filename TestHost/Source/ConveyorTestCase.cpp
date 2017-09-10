505bf42256f60351cf6d3cc8722ece681d7975c6
Jan Schwers <JanSchwers@gmail.com>
2017-09-10 17:45:17 +0200

    Moved xcode workspace and projects into their own directories.

--- TestHost/Source/ConveyorTestCase.cpp
+++ TestHost/Source/ConveyorTestCase.cpp
@@ -242,5 +242,5 @@ TEST(PerformanceTest_conveyor, copy)
     }
 
     auto&& duration = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - start);
-    EXPECT_GT(std::chrono::milliseconds(17000), duration) << std::to_string(duration.count());
+    EXPECT_GT(std::chrono::milliseconds(20000), duration) << std::to_string(duration.count());
 }
